4816420660b4c9f31907a9ee91fb7dd3b8da6e85
Sukhman Hara <shara1@myseneca.ca>
2024-04-19 17:54:02 -0400

    Remove all files

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomePageView, AboutPageView, ArtistPageView, AlbumPageView
-
-urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about"),
-    path("<int:artistid>/", ArtistPageView.as_view(), name="artist"),
-    path("<int:artistid>/<int:albumid>/", AlbumPageView.as_view(), name="album")
-]
